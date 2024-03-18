69d4c66b34919633b44a2727feb75d1810d0c8c7
tendersanic <161989346+tendersanic@users.noreply.github.com>
2024-03-18 12:02:01 +0530

    Create upcloud.ts

---
+++ scrape/upcloud.ts
@@ -0,0 +1,91 @@
+const puppeteer = require('puppeteer-extra');
+const chrome = require('@sparticuz/chromium');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+
+export default async (req: any, res: any) => {
+  puppeteer.use(StealthPlugin())
+  let {body,method} = req
+
+  // Some header shits
+  if (method !== 'POST') {
+    res.setHeader('Access-Control-Allow-Credentials', true)
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+    )
+    return res.status(200).end()
+  }
+
+  // Some checks...
+  if (!body) return res.status(400).end(`No body provided`)
+  if (typeof body === 'object' && !body.url) return res.status(400).end(`No url provided`)
+  const isProd = process.env.NODE_ENV === 'production'
+
+  // create browser based on ENV
+  let browser;
+  if (isProd) {
+    browser = await puppeteer.launch({
+      args: chrome.args,
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath(),
+      headless: 'new',
+      ignoreHTTPSErrors: true
+    })
+  } else {
+    browser = await puppeteer.launch({
+      headless: 'new',
+      executablePath: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+    })
+  }
+  
+  const browser = await puppeteer.launch({
+      args: chrome.args,
+      defaultViewport: chrome.defaultViewport,
+      executablePath: await chrome.executablePath(),
+      headless:true,
+      ignoreHTTPSErrors: true,
+  });
+  const page = await browser.newPage();
+  await page.setRequestInterception(true);
+
+  // Set headers,else wont work.
+  await page.setExtraHTTPHeaders({ 'Referer': 'https://flixhq.to/' });
+  
+  const logger:string[] = [];
+  const finalResponse:{source:string,subtitle:string[]} = {source:'',subtitle:[]}
+  
+  page.on('request', async (interceptedRequest) => {
+    await (async () => {
+      logger.push(interceptedRequest.url());
+      if (interceptedRequest.url().includes('.m3u8')) finalResponse.source = interceptedRequest.url();
+      interceptedRequest.continue();
+    })();
+  });
+  
+  try {
+    const [req] = await Promise.all([
+      page.waitForRequest(req => req.url().includes('.m3u8'), { timeout: 30000 }),
+      page.goto('https://rabbitstream.net/v2/embed-4/J8fjXoGMl7rw?z=&_debug=true', { waitUntil: 'domcontentloaded' }),
+    ]);
+  } catch (error) {
+    return res.status(500).end(`Server Error,check the params.`)
+  }
+  await browser.close();
+
+  // Response headers.
+  res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate')
+  res.setHeader('Content-Type', 'image/png')
+  // CORS
+  // res.setHeader('Access-Control-Allow-Headers', '*')
+  res.setHeader('Access-Control-Allow-Credentials', true)
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
+  res.json(finalResponse);
+};
+
